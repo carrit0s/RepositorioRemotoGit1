a813f2d1d9dced2ccd70bfb15bf0753a82080a34
DAW <DAW@DESKTOP-U4FTC0L>
2023-05-05 14:00:01 +0200

    Segundo comit

--- Proyecto1/src/paquete1/Principal.java
+++ Proyecto1/src/paquete1/Principal.java
@@ -3,6 +3,7 @@ package paquete1;
 public class Principal {
 	public static void main(String[] args) {
 		System.out.println("Ejemplo de control de versiones con Git");
+		System.out.println("Ejemplo 2 de control de versiones");
 	}
 
 }
